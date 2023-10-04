eb116312926dad856339339e54c8d35447b01383
Katzumi Urdininea <katurdininea02@gmail.com>
2023-10-04 19:01:40 -0400

    Parcial1 - Commit2

--- untitled/src/main/java/ejercicio1/CalcularDescuento.java
+++ untitled/src/main/java/ejercicio1/CalcularDescuento.java
@@ -1,15 +1,15 @@
 package ejercicio1;
 
 public class CalcularDescuento {
-   public int devDescuento (int sueldo) throws Exception{
-       if(sueldo>0 && sueldo<= 2000){
-           return sueldo;
-       }else if (sueldo>2000 && sueldo<=4000){
-           return sueldo - sueldo*5/100;
-       }else if (sueldo>40000){
-           return sueldo - sueldo*15/100;
-       }else{
-           throw new Exception("ERROR, el sueldo que se ingreso es incorrecto");
-       }
-   }
+    public int devDescuento (int sueldo) throws Exception{
+        if(sueldo>0 && sueldo<=2000){
+            return sueldo;
+        }else if(sueldo>2000 && sueldo<=4000){
+            return sueldo - sueldo*5/100;
+        }else if (sueldo>4000){
+            return sueldo - sueldo*15/100;
+        }else {
+            throw new Exception("ERROR, el sueldo ingresado es incorrecto");
+        }
+    }
 }
--- untitled/src/test/java/testEjercicio1/TestDescuento.java
+++ untitled/src/test/java/testEjercicio1/TestDescuento.java
@@ -1,8 +1,8 @@
 package testEjercicio1;
 
-import org.junit.jupiter.api.Test;
 import ejercicio1.CalcularDescuento;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -10,13 +10,33 @@ public class TestDescuento {
     @ParameterizedTest
     @CsvSource({
             "0,0",
-            "1.1",
+            "1,1",
             "2,2",
 
             "999,999",
             "1000,1000",
             "1001,1001",
 
+            "1999,1999",
+            "2000,2000",
+            "2001,1901",
 
+            "2999,2850",
+            "3000,2850",
+            "3001,2851",
+
+            "3999,3800",
+            "4000,3800",
+            "4001,3401"
     })
+
+    public void calDescuento(int sueldo, int expected) throws Exception{
+        CalcularDescuento calcularDescuento = new CalcularDescuento();
+        if(sueldo<1){
+            Assertions.assertThrows(Exception.class, ()->{calcularDescuento.devDescuento(sueldo);});
+        }else {
+            int actualResult = calcularDescuento.devDescuento(sueldo);
+            Assertions.assertEquals(expected, actualResult, "ERROR en el descuento");
+        }
+    }
 }
